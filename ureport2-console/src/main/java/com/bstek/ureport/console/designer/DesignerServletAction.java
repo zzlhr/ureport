5000949c6d2fd817bf3129a517a327fbd8a29bc9
zzlhr <2388399752@qq.com>
2020-09-01 13:22:29 +0800

    打包处理

--- ureport2-console/src/main/java/com/bstek/ureport/console/designer/DesignerServletAction.java
+++ ureport2-console/src/main/java/com/bstek/ureport/console/designer/DesignerServletAction.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2017 Bstek
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -55,162 +55,190 @@ import com.bstek.ureport.provider.report.ReportProvider;
  * @since 2017年1月25日
  */
 public class DesignerServletAction extends RenderPageServletAction {
-	private ReportRender reportRender;
-	private ReportParser reportParser;
-	private List<ReportProvider> reportProviders=new ArrayList<ReportProvider>();
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String method=retriveMethod(req);
-		if(method!=null){
-			invokeMethod(method, req, resp);
-		}else{
-			VelocityContext context = new VelocityContext();
-			context.put("contextPath", req.getContextPath());
-			resp.setContentType("text/html");
-			resp.setCharacterEncoding("utf-8");
-			Template template=ve.getTemplate("ureport-html/designer.html","utf-8");
-			PrintWriter writer=resp.getWriter();
-			template.merge(context, writer);
-			writer.close();
-		}
-	}
-	public void scriptValidation(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String content=req.getParameter("content");
-		ANTLRInputStream antlrInputStream=new ANTLRInputStream(content);
-		ReportParserLexer lexer=new ReportParserLexer(antlrInputStream);
-		CommonTokenStream tokenStream=new CommonTokenStream(lexer);
-		ReportParserParser parser=new ReportParserParser(tokenStream);
-		ScriptErrorListener errorListener=new ScriptErrorListener();
-		parser.removeErrorListeners();
-		parser.addErrorListener(errorListener);
-		parser.expression();
-		List<ErrorInfo> infos=errorListener.getInfos();
-		writeObjectToJson(resp, infos);
-	}
-	
-	public void conditionScriptValidation(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String content=req.getParameter("content");
-		ANTLRInputStream antlrInputStream=new ANTLRInputStream(content);
-		ReportParserLexer lexer=new ReportParserLexer(antlrInputStream);
-		CommonTokenStream tokenStream=new CommonTokenStream(lexer);
-		ReportParserParser parser=new ReportParserParser(tokenStream);
-		ScriptErrorListener errorListener=new ScriptErrorListener();
-		parser.removeErrorListeners();
-		parser.addErrorListener(errorListener);
-		parser.expr();
-		List<ErrorInfo> infos=errorListener.getInfos();
-		writeObjectToJson(resp, infos);
-	}
-	
-	
-	public void parseDatasetName(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String expr=req.getParameter("expr");
-		ANTLRInputStream antlrInputStream=new ANTLRInputStream(expr);
-		ReportParserLexer lexer=new ReportParserLexer(antlrInputStream);
-		CommonTokenStream tokenStream=new CommonTokenStream(lexer);
-		ReportParserParser parser=new ReportParserParser(tokenStream);
-		parser.removeErrorListeners();
-		DatasetContext ctx=parser.dataset();
-		String datasetName=ctx.Identifier().getText();
-		Map<String,String> result=new HashMap<String,String>();
-		result.put("datasetName", datasetName);
-		writeObjectToJson(resp, result);
-	}
-	public void savePreviewData(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String content=req.getParameter("content");
-		content=decodeContent(content);
-		InputStream inputStream=IOUtils.toInputStream(content,"utf-8");
-		ReportDefinition reportDef=reportParser.parse(inputStream,"p");
-		reportRender.rebuildReportDefinition(reportDef);
-		IOUtils.closeQuietly(inputStream);
-		TempObjectCache.putObject(PREVIEW_KEY, reportDef);
-	}
-	
-	public void loadReport(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String file=req.getParameter("file");
-		if(file==null){
-			throw new ReportDesignException("Report file can not be null.");
-		}
-		file=ReportUtils.decodeFileName(file);
-		Object obj=TempObjectCache.getObject(file);
-		if(obj!=null && obj instanceof ReportDefinition){
-			ReportDefinition reportDef=(ReportDefinition)obj;
-			TempObjectCache.removeObject(file);
-			writeObjectToJson(resp, new ReportDefinitionWrapper(reportDef));
-		}else{
-			ReportDefinition reportDef=reportRender.parseReport(file);
-			writeObjectToJson(resp, new ReportDefinitionWrapper(reportDef));			
-		}
-	}
-	
-	public void deleteReportFile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String file=req.getParameter("file");
-		if(file==null){
-			throw new ReportDesignException("Report file can not be null.");
-		}
-		ReportProvider targetReportProvider=null;
-		for(ReportProvider provider:reportProviders){
-			if(file.startsWith(provider.getPrefix())){
-				targetReportProvider=provider;
-				break;
-			}
-		}
-		if(targetReportProvider==null){
-			throw new ReportDesignException("File ["+file+"] not found available report provider.");
-		}
-		targetReportProvider.deleteReport(file);
-	}
-	
-	
-	public void saveReportFile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String file=req.getParameter("file");
-		file=ReportUtils.decodeFileName(file);
-		String content=req.getParameter("content");
-		content=decodeContent(content);
-		ReportProvider targetReportProvider=null;
-		for(ReportProvider provider:reportProviders){
-			if(file.startsWith(provider.getPrefix())){
-				targetReportProvider=provider;
-				break;
-			}
-		}
-		if(targetReportProvider==null){
-			throw new ReportDesignException("File ["+file+"] not found available report provider.");
-		}
-		targetReportProvider.saveReport(file, content);
-		InputStream inputStream=IOUtils.toInputStream(content,"utf-8");
-		ReportDefinition reportDef=reportParser.parse(inputStream, file);
-		reportRender.rebuildReportDefinition(reportDef);
-		CacheUtils.cacheReportDefinition(file, reportDef);
-		IOUtils.closeQuietly(inputStream);
-	}
-	
-	public void loadReportProviders(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		writeObjectToJson(resp, reportProviders);
-	}
-	
-	public void setReportRender(ReportRender reportRender) {
-		this.reportRender = reportRender;
-	}
-	
-	public void setReportParser(ReportParser reportParser) {
-		this.reportParser = reportParser;
-	}
-	
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext)throws BeansException {
-		super.setApplicationContext(applicationContext);
-		Collection<ReportProvider> providers=applicationContext.getBeansOfType(ReportProvider.class).values();
-		for(ReportProvider provider:providers){
-			if(provider.disabled() || provider.getName()==null){
-				continue;
-			}
-			reportProviders.add(provider);
-		}
-	}
+    private ReportRender reportRender;
+    private ReportParser reportParser;
+    private List<ReportProvider> reportProviders = new ArrayList<>();
 
-	@Override
-	public String url() {
-		return "/designer";
-	}
+    public ReportRender getReportRender() {
+        return reportRender;
+    }
+
+    public ReportParser getReportParser() {
+        return reportParser;
+    }
+
+    public List<ReportProvider> getReportProviders() {
+        return reportProviders;
+    }
+
+    public void setReportProviders(List<ReportProvider> reportProviders) {
+        this.reportProviders = reportProviders;
+    }
+
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method = retriveMethod(req);
+        if (method != null) {
+            invokeMethod(method, req, resp);
+        } else {
+            VelocityContext context = new VelocityContext();
+            context.put("contextPath", req.getContextPath());
+            resp.setContentType("text/html");
+            resp.setCharacterEncoding("utf-8");
+            Template template = ve.getTemplate("ureport-html/designer.html", "utf-8");
+            PrintWriter writer = resp.getWriter();
+            template.merge(context, writer);
+            writer.close();
+        }
+    }
+
+    public void scriptValidation(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String content = req.getParameter("content");
+        ANTLRInputStream antlrInputStream = new ANTLRInputStream(content);
+        ReportParserLexer lexer = new ReportParserLexer(antlrInputStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        ReportParserParser parser = new ReportParserParser(tokenStream);
+        ScriptErrorListener errorListener = new ScriptErrorListener();
+        parser.removeErrorListeners();
+        parser.addErrorListener(errorListener);
+        parser.expression();
+        List<ErrorInfo> infos = errorListener.getInfos();
+        writeObjectToJson(resp, infos);
+    }
+
+    public void conditionScriptValidation(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String content = req.getParameter("content");
+        ANTLRInputStream antlrInputStream = new ANTLRInputStream(content);
+        ReportParserLexer lexer = new ReportParserLexer(antlrInputStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        ReportParserParser parser = new ReportParserParser(tokenStream);
+        ScriptErrorListener errorListener = new ScriptErrorListener();
+        parser.removeErrorListeners();
+        parser.addErrorListener(errorListener);
+        parser.expr();
+        List<ErrorInfo> infos = errorListener.getInfos();
+        writeObjectToJson(resp, infos);
+    }
+
+
+    public void parseDatasetName(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String expr = req.getParameter("expr");
+        ANTLRInputStream antlrInputStream = new ANTLRInputStream(expr);
+        ReportParserLexer lexer = new ReportParserLexer(antlrInputStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        ReportParserParser parser = new ReportParserParser(tokenStream);
+        parser.removeErrorListeners();
+        DatasetContext ctx = parser.dataset();
+        String datasetName = ctx.Identifier().getText();
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("datasetName", datasetName);
+        writeObjectToJson(resp, result);
+    }
+
+    public void savePreviewData(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String content = req.getParameter("content");
+        content = decodeContent(content);
+        InputStream inputStream = IOUtils.toInputStream(content, "utf-8");
+        ReportDefinition reportDef = reportParser.parse(inputStream, "p");
+        reportRender.rebuildReportDefinition(reportDef);
+        IOUtils.closeQuietly(inputStream);
+        TempObjectCache.putObject(PREVIEW_KEY, reportDef);
+    }
+
+    public void loadReport(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String file = req.getParameter("file");
+        if (file == null) {
+            throw new ReportDesignException("Report file can not be null.");
+        }
+        file = ReportUtils.decodeFileName(file);
+        Object obj = TempObjectCache.getObject(file);
+        if (obj instanceof ReportDefinition) {
+            ReportDefinition reportDef = (ReportDefinition) obj;
+            TempObjectCache.removeObject(file);
+            writeObjectToJson(resp, new ReportDefinitionWrapper(reportDef));
+        } else {
+            ReportDefinition reportDef = reportRender.parseReport(file);
+            writeObjectToJson(resp, new ReportDefinitionWrapper(reportDef));
+        }
+    }
+
+    public void deleteReportFile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String file = req.getParameter("file");
+        if (file == null) {
+            throw new ReportDesignException("Report file can not be null.");
+        }
+        ReportProvider targetReportProvider = null;
+        for (ReportProvider provider : reportProviders) {
+            if (file.startsWith(provider.getPrefix())) {
+                targetReportProvider = provider;
+                break;
+            }
+        }
+        if (targetReportProvider == null) {
+            throw new ReportDesignException("File [" + file + "] not found available report provider.");
+        }
+        targetReportProvider.deleteReport(file);
+    }
+
+
+    public void saveReportFile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String file = req.getParameter("file");
+        file = ReportUtils.decodeFileName(file);
+        String content = req.getParameter("content");
+        content = decodeContent(content);
+
+        // 通过企业代号查找provider
+        ReportProvider targetReportProvider = null;
+        for (ReportProvider provider : reportProviders) {
+            if (file.startsWith(provider.getPrefix())) {
+                targetReportProvider = provider;
+                break;
+            }
+        }
+        if (targetReportProvider == null) {
+            throw new ReportDesignException("File [" + file + "] not found available report provider.");
+        }
+        targetReportProvider.saveReport(file, content);
+        InputStream inputStream = IOUtils.toInputStream(content, "utf-8");
+        ReportDefinition reportDef = reportParser.parse(inputStream, file);
+        reportRender.rebuildReportDefinition(reportDef);
+        CacheUtils.cacheReportDefinition(file, reportDef);
+        IOUtils.closeQuietly(inputStream);
+    }
+
+    public void loadReportProviders(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        reportProviderRegister();
+        writeObjectToJson(resp, reportProviders);
+    }
+
+    public void setReportRender(ReportRender reportRender) {
+        this.reportRender = reportRender;
+    }
+
+    public void setReportParser(ReportParser reportParser) {
+        this.reportParser = reportParser;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        super.setApplicationContext(applicationContext);
+        reportProviderRegister();
+    }
+
+
+    private void reportProviderRegister() {
+        reportProviders = new ArrayList<>();
+        Collection<ReportProvider> providers = applicationContext.getBeansOfType(ReportProvider.class).values();
+        for (ReportProvider provider : providers) {
+            if (provider.disabled() || provider.getName() == null) {
+                continue;
+            }
+            reportProviders.add(provider);
+        }
+    }
+
+    @Override
+    public String url() {
+        return "/designer";
+    }
 }
